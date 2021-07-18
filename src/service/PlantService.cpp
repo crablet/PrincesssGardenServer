36f34d2d6ac68a8ba10e036050f3affdccfdaf14
crablet <love4uandgzfc@gmail.com>
2021-07-19 02:53:53 +0800

    feat: 创建植物接口

--- src/service/PlantService.cpp
+++ src/service/PlantService.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "PlantService.h"
+
+oatpp::Object<PlantStatusDto> PlantService::createPlant(const oatpp::Object<PlantStatusDto> &dto)
+{
+    auto dbResult = database->createPlant(dto);
+    OATPP_ASSERT_HTTP(dbResult->isSuccess(), Status::CODE_500, dbResult->getErrorMessage());
+
+    // todo: 补充完整这个函数
+    return {};
+}
--- src/service/PlantService.h
+++ src/service/PlantService.h
@@ -5,10 +5,22 @@
 #ifndef PRINCESSSGARDENSERVER_PLANTSERVICE_H
 #define PRINCESSSGARDENSERVER_PLANTSERVICE_H
 
+#include "oatpp/web/protocol/http/Http.hpp"
+#include "oatpp/core/macro/component.hpp"
+
+#include "dto/DTOs.hpp"
+#include "db/PlantDb.hpp"
 
 class PlantService
 {
+private:
+    using Status = oatpp::web::protocol::http::Status;
 
+private:
+    OATPP_COMPONENT(std::shared_ptr<PlantDb>, database);
+
+public:
+    oatpp::Object<PlantStatusDto> createPlant(const oatpp::Object<PlantStatusDto> &dto);
 };
 
 
